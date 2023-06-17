ab58ba00231e8e06e7c1507a4f0d684198b348f8
surajkarki66 <suraj.karki500@protonmail.com>
2023-06-17 22:14:43 +0545

    feat: add data ingestion pipeline

---
+++ src/Medileaf_AI/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,30 @@
+from Medileaf_AI.config.configuration import ConfigurationManager
+from Medileaf_AI.components.data_ingestion import DataIngestion
+from Medileaf_AI.utils.common import logger
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(
+            f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
