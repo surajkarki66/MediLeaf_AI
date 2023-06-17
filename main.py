2aeac0537c1b5fa0a39a0d0737fa0b08a7200208
surajkarki66 <suraj.karki500@protonmail.com>
2023-06-17 22:16:41 +0545

    feat: finished data ingestion

---
+++ main.py
@@ -0,0 +1,13 @@
+from Medileaf_AI import logger
+from Medileaf_AI.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
