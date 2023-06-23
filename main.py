5d4a8cdf6554f51a161c7670e4e1f8e0fe74c313
surajkarki66 <suraj.karki500@protonmail.com>
2023-06-23 15:55:04 +0545

    feat: finished prepare base model pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Medileaf_AI import logger
 from Medileaf_AI.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Medileaf_AI.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -11,3 +12,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/Medileaf_AI/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,30 @@
+from Medileaf_AI.config.configuration import ConfigurationManager
+from Medileaf_AI.components.prepare_base_model import PrepareBaseModel
+from Medileaf_AI.utils.common import logger
+
+STAGE_NAME = "Prepare base model"
+
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(
+            f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
