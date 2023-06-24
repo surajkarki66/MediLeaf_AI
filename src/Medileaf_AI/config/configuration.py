5574511d6c9f629702e7cb8035b74675a4cc2860
surajkarki66 <suraj.karki500@protonmail.com>
2023-06-24 10:37:03 +0545

    feat: completed prepare callbacks

--- src/Medileaf_AI/config/configuration.py
+++ src/Medileaf_AI/config/configuration.py
@@ -1,6 +1,8 @@
+import os
+
 from Medileaf_AI.constants import *
 from Medileaf_AI.utils.common import read_yaml, create_directories
-from Medileaf_AI.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from Medileaf_AI.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig
 
 
 class ConfigurationManager:
@@ -45,3 +47,19 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
