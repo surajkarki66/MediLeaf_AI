4c932bbd2a5e68ad0b24532d31bc482359513313
surajkarki <suraj.karki500@protonmail.com>
2024-01-09 23:33:11 +0100

    add fastapi and uvicorn

--- service.py
+++ service.py
@@ -1,25 +1,6 @@
-import bentoml
-import rembg
+import os
+import uvicorn
 
-from bentoml.io import Image, JSON
-
-from MediLeaf_AI.utils.common import image_to_array, map_predictions_to_species_with_proba, add_white_background
-
-BENTO_MODEL_TAG = "inceptionv3:4yxengrd62vvm4a4"
-
-classifier_runner = bentoml.tensorflow.get(BENTO_MODEL_TAG).to_runner()
-
-medileaf_service = bentoml.Service("medileaf_classifier", runners=[classifier_runner])
-
-session = rembg.new_session("u2netp")
-
-
-@medileaf_service.api(input=Image(), output=JSON())
-async def classify(input_image):
-    img = add_white_background(session, input_image, size=(1600, 1200))
-    img_arr = image_to_array(img)
-    result =  await classifier_runner.async_run(img_arr)
-    prediction_response = map_predictions_to_species_with_proba(result, "./classes.json")
-    
-    return prediction_response
-
+if __name__ == "__main__":
+	port = int(os.environ.get('PORT', 5000))
+	uvicorn.run("app.api:app", host="0.0.0.0", port=port)
