c76644694e7fd517bd514d549a8b27324f9dedb9
surajkarki <suraj.karki500@protonmail.com>
2024-01-11 21:55:39 +0100

    feat: port changes

--- service.py
+++ service.py
@@ -2,5 +2,5 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 8000))
+	port = int(os.environ.get('PORT', 8080))
 	uvicorn.run("app.api:app", host="0.0.0.0", port=port)
